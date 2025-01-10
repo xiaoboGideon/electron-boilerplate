7ac738cae4b4ba75b56a6e809772da6c8b535c09
kimizuy <kimizuyamasaki@gmail.com>
2025-01-10 22:22:06 +0900

    chore: remove unnecessary code

--- src/ipc-main-listeners/fetch-users.ts
+++ src/ipc-main-listeners/fetch-users.ts
@@ -3,10 +3,5 @@ import { db } from "../utils/db";
 import { type IpcMainListener } from ".";
 
 export const fetchUsers = (async (): Promise<(typeof users.$inferSelect)[]> => {
-  db.insert(users)
-    .values({ id: 1, name: "John Doe" })
-    .onConflictDoNothing({ target: users.id })
-    .run();
-
   return await db.select().from(users);
 }) satisfies IpcMainListener;
